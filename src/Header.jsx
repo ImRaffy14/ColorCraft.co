249bddb14a021d1eb86115ed846b4c35076cad8b
ImRaffy14 <raffysolis123@gmail.com>
2024-04-13 21:36:39 +0800

    added: some updates

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,5 @@
 import Logo from './assets/Logo.png'
 import React, { useState, useEffect } from 'react';
-import { FaPaintRoller } from "react-icons/fa";
-import { FaPaintBrush } from "react-icons/fa";
-import { GiSpray } from "react-icons/gi";
 import { Link } from 'react-scroll';
 import { Link as Links} from 'react-router-dom'
 
@@ -35,7 +32,7 @@ function Header() {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </div>
           <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-black">
-            <li className="mr-4"><Links to="/ColorCraft.co/Products">Products</Links></li>
+            <li className="mr-4"><Links to="/ColorCraft.co/IllumiGlow">Products</Links></li>
             <li><Link to="about-us" smooth={true} duration={500}>About Us</Link></li>
             <li><a>Contact Us</a></li>
             <li><a>FAQ's</a></li>
@@ -48,7 +45,7 @@ function Header() {
       </div>
       <div className="navbar-center hidden md:flex ">
         <ul className="menu menu-horizontal px-1">
-          <li className="mr-4"><Links to="/ColorCraft.co/Products">Products</Links></li>
+          <li className="mr-4"><Links to="/ColorCraft.co/IllumiGlow">Products</Links></li>
           <li className="mr-4"><Link activeClass="active" spy={true} to="about-us" smooth={true} duration={500}>About Us</Link></li>
           <li className="mr-4"><a>Contact Us</a></li>
           <li className="mr-4"><a>FAQ's</a></li>
